46e2ae9407ecf5bdc58c0c045c1bd8109bcee79b
Kawany <91629758+kwvalera@users.noreply.github.com>
2025-06-03 19:40:22 -0300

    adicionando integração mailtrap
    - Commit solicitado por Nicole

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import sqlite3
 import datetime
+import smtplib
+from email.message import EmailMessage
 
 # Caminho do banco
 DB_PATH = './banco/projeto_bradesco.db'
+
+# Configurações do Mailtrap SMTP
+SMTP_HOST = 'sandbox.smtp.mailtrap.io'
+SMTP_PORT = 587
+SMTP_USER = ''
+SMTP_PASS = ''
+EMAIL_DESTINO = 'teste_prefeitura@exemplo.com'
 
 class VerificadorEmail:
     def __init__(self, db_path):
@@ -12,6 +21,18 @@ class VerificadorEmail:
         con = sqlite3.connect(self.db_path)
         con.execute("PRAGMA foreign_keys = ON")
         return con
+    
+    def enviar_alerta(self, remetente):
+        msg = EmailMessage()
+        msg['Subject'] = 'Alerta de e-mail suspeito'
+        msg['From'] = 'alerta@sistema_prefeitura.com'
+        msg['To'] = EMAIL_DESTINO
+        msg.set_content(f'Você recebeu um email suspeito: {remetente}')
+
+        with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as smtp:
+            smtp.starttls()
+            smtp.login(SMTP_USER, SMTP_PASS)
+            smtp.send_message(msg)
 
     def verificar_email(self, remetente: str):
         con = self.conectar_banco()
@@ -34,6 +55,7 @@ class VerificadorEmail:
                 INSERT INTO LogHis (DatLog, CodStatus, Response, ObsLog)
                 VALUES (?, ?, ?, ?)
             """, (now, 403, None, f"E-mail {remetente} não reconhecido."))
+            self.enviar_alerta(remetente)
 
         con.commit()
         cursor.close()
@@ -45,7 +67,8 @@ verificador = VerificadorEmail(DB_PATH)
 
 emails_recebidos = [
     "contato@empresa1.com",
-    "fraude@naoexiste.com"
+    "fraude@naoexiste.com",
+    "email_maluco@nao.com"
 ]
 
 for email in emails_recebidos:
